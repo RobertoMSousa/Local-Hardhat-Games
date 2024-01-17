2704e0b941b1920def9e35fcf3e8dbdfd795991f
Roberto Sousa <beto.sousa22@gmail.com>
2024-01-17 16:48:19 +0000

    passed  challenges

--- scripts/win.js
+++ scripts/win.js
@@ -1,16 +1,21 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9A676e781A523b5d0C0e43731313A708CB607508";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
+    
+
+    const txAllowence = await game.giveMeAllowance(10000);
+    await txAllowence.wait();
+    const txMint = await game.mint(10000);
+    await txMint.wait();
+    
     const tx = await game.win();
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
+    
     const receipt = await tx.wait();
     console.log(receipt);
 }
